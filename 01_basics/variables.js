27183789e6ee05720248e89b73fdab441923987e
Satyajit0003 <149392619+Satyajit0003@users.noreply.github.com>
2024-04-01 15:09:39 +0000

    this code is for const,var,let keyword

---
+++ 01_basics/variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail="satyajit@gmail.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+let accountState;
+
+//accountId=2
+accountEmail="ram@gmail.com"
+accountPassword="bapi"
+accountCity="padampur"
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
