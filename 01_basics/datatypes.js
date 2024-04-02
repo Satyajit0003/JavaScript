e026527d1f9d31b295c9dced4d3b8c6e63888b74
Satyajit0003 <149392619+Satyajit0003@users.noreply.github.com>
2024-04-02 13:46:38 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all JS code as newer version
+
+//alert("hello") //we are using nodejs, not browser
+
+console.log(3+3); console.log("hello") //it is not a good readability 
+//code readability should be high
+
+let name="Satyajit"
+let age=20
+let isLoggedIn=false
+
+//number=>2 to power 53
+//bigint
+//string=>"" or ''
+//boolean=>true/false
+//null=>standalone value->represention of a empty value
+//undefined=>
+//symbol=>unique
+
+//type of
+console.log("abc");  //"abc"->string
+console.log(23);//23->number
+console.log(null);//null->object
+console.log(undefined);//undefined->undefined
+
+//object
+console.log(typeof undefined)
--- 01_basics/variables.js
+++ 01_basics/variables.js
@@ -10,4 +10,9 @@ accountPassword="bapi"
 accountCity="padampur"
 console.log(accountId);
 
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
 console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
