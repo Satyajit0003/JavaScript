c9f80d2de089ae214dc8ff5521316f4fcc9ecf95
Satyajit0003 <149392619+Satyajit0003@users.noreply.github.com>
2024-04-02 14:45:11 +0000

    operations

--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -1,10 +1,11 @@
+//*******conversions******** */
 let score=true
-console.log(typeof score);
-console.log(typeof (score));
+//console.log(typeof score);
+//console.log(typeof (score));
 
 let valueInNumber=Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber);
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber);
 //if score is null, after conver to Number the output give 0
 //score=undefined then it give output nan
 //true->1 /false->0
@@ -19,11 +20,11 @@ console.log(valueInNumber);
 //NaN->not a number
 //console.log(typeof NaN) ->nan
 
-let isLoggedIn=""
+let isLoggedIn=7
 
 let booleanIsLoggedIn=Boolean(isLoggedIn)
-console.log(typeof booleanIsLoggedIn)
-console.log(booleanIsLoggedIn)
+//console.log(typeof booleanIsLoggedIn)
+//console.log(booleanIsLoggedIn)
 
 //1=>true
 //0=>false
@@ -35,6 +36,38 @@ console.log(booleanIsLoggedIn)
 let someNumber=1
 
 let stringSomeNumber=String(someNumber)
-console.log(typeof stringSomeNumber)
-console.log(stringSomeNumber)
+//console.log(typeof stringSomeNumber)
+//console.log(stringSomeNumber)
 
+
+/********Operations********** */
+
+let value=3
+let negValue=-value
+//console.log(negValue);
+
+//console.log(Object)
+let str1="Hello"
+let str2=" Satyajit"
+// console.log(str1+str2);
+
+// console.log("1"+2);//12
+// console.log(2+"1");//21
+// console.log("1"+2+2);//122
+// console.log(2+2+"1");//41
+
+// console.log(+true);
+// console.log(+"");
+
+let num1,num2,num3
+
+num1=num2=num3=2+2
+
+let gameCounter=100
+++gameCounter
+console.log(gameCounter);
+
+let p=10
+console.log(--p)
+let q=10
+console.log(q--)
